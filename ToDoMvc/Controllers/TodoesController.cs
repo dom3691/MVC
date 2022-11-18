2b01267fb6903f72d8a4c7e951f3367327b466f0
My Name <efioesien@gmail.com>
2022-11-18 22:06:14 +0100

    first commit

---
+++ ToDoMvc/Controllers/TodoesController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoMvc.Models;
+
+namespace ToDoMvc.Controllers
+{
+    public class TodoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Todoes
+        public ActionResult Index()
+        {
+            string currentUserId = User.Identity.GetUserId();
+            ApplicationUser currentUser = db.Users.FirstOrDefault
+                (x => x.Id ==currentUserId);
+            return View(db.ToDos.ToList().Where (x=> x.User == currentUser));    
+        }
+
+        // GET: Todoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.ToDos.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // GET: Todoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,IsDone")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                string currentUserId = User.Identity.GetUserId();
+                ApplicationUser currentUser = db.Users.FirstOrDefault
+                    (x => x.Id == currentUserId);
+                todo.User = currentUser;
+                db.ToDos.Add(todo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        // GET: Todoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.ToDos.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description,IsDone")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+        // GET: Todoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.ToDos.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Todo todo = db.ToDos.Find(id);
+            db.ToDos.Remove(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ToDoMvc/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoMvc.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        //cv
+        public string Description { get; set; } 
+        public bool IsDone { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public static implicit operator Todo(ApplicationUser v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
